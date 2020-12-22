64f229b62cfc5fb4d5596804bb326e5c670a4003
Amir Rehman <aamirnorth@gmail.com>
2020-12-22 12:08:55 +0400

    first commit

---
+++ precache-manifest.0441cad64bffd454bacb8c970ea33279.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8938e3ac66e1aa307c26e75418e3367e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eab864ec884d7edfc881",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "4e7aaa6add543bdfef93",
+    "url": "/static/css/main.8fe6be9a.chunk.css"
+  },
+  {
+    "revision": "eab864ec884d7edfc881",
+    "url": "/static/js/2.361b3123.chunk.js"
+  },
+  {
+    "revision": "f1e5c444d64bf1db9cc5e3b16d2045b8",
+    "url": "/static/js/2.361b3123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7aaa6add543bdfef93",
+    "url": "/static/js/main.d2a21d6d.chunk.js"
+  },
+  {
+    "revision": "d8fd68c986c9c6c37b1d",
+    "url": "/static/js/runtime-main.78ed9f41.js"
+  },
+  {
+    "revision": "1d3261af86b48af5ebc4094364964314",
+    "url": "/static/media/bg.1d3261af.jpg"
+  },
+  {
+    "revision": "f1331fefa6f366a98847863a38a2ead9",
+    "url": "/static/media/loading.f1331fef.gif"
+  }
+]);
